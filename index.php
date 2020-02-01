1eeab8ce738a3f471a4a7b5821bd62cc76c7ef33
pranudech <pranudech@gmail.com>
2020-02-01 23:34:30 +0700

    #Update

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 //  $LINK_RESPOND = 'http://app01.kwanseeds.com/page/index.php';
  $LINK_RESPOND = 'http://localhost/app/page/index.php';
  header( 'location: '.$LINK_RESPOND );
- exit(0);
+ exit(0); 
 ?>
